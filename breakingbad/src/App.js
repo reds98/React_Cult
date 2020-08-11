4857c52b9d618df9eac0ec41f50b4a235fcc0200
Sahid <sahid.rojas64@gmail.com>
2020-08-10 18:48:06 -0600

    [ADD]Added a simple project for introduccing api with react

---
+++ breakingbad/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect} from "react";
+import logo from "./logo.svg";
+import styled from "@emotion/styled";
+import Frase from "./components/Frase";
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover{
+    cursor: pointer;
+    background-size:400px;
+  }
+`;
+
+
+function App() {
+  //State de frases
+  const [frase, guardarFrase] = useState({});
+  const consultarApi = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+  
+    guardarFrase(frase[0]);
+  };
+
+  //Cragra una frase al inicio de la pagina
+  useEffect(()=>{
+    consultarApi();
+  },[])
+  return (
+    <Contenedor>
+      <Frase
+      frase={frase}/>
+      <Boton onClick={consultarApi}>Obtener Frase</Boton>
+    </Contenedor>
+  );
+}
+
+export default App;
